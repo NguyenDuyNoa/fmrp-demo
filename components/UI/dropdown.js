ff3cd5c4c19f780075013e37e2cebd8a2c8331bf
Paul <trantuanphong2410@gmail.com>
2023-03-01 21:19:35 +0700

    .

--- components/UI/dropdown.js
+++ components/UI/dropdown.js
@@ -18,10 +18,10 @@ const Dropdown = (props) => {
                 <div className="w-auto">
                     <div className="bg-white py-2 px-0.5 rounded-t justify-between flex divide-x divide-[#DDDDE2]">
                         {props.data?.map((e) => 
-                            <div className={`${e.title ? "px-7 py-3" : "px-1"} space-y-4 min-w-[150px]`} key={e.title.toString()}>
+                            <div className={`${e.title ? "px-7 py-3" : "px-1"} space-y-4 min-w-[150px]`} key={e.title?.toString()}>
                                 {e.title && <h3 className="text-[20px] font-medium px-3">{e.title}</h3>}
                                 {e.sub?.map((ce) => 
-                                    <div className="space-y-0.5" key={ce.title.toString()}>
+                                    <div className="space-y-0.5" key={ce.title?.toString()}>
                                         {ce.link ? 
                                             <Link title={ce.title} href={ce.link} className="flex items-center space-x-2 mb-2 px-3 py-2 rounded hover:bg-[#ececee87] text-[#344054]">
                                                 <Image src={ce?.img} width={18} height={18} quality={100} className="object-contain" loading="lazy" crossOrigin="anonymous" blurDataURL="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
@@ -38,7 +38,7 @@ const Dropdown = (props) => {
                                             </React.Fragment>
                                         }
                                         {ce.items?.map((e) => 
-                                            <Link href={e.link ? e.link : "#"} title={e.name} className="outline-none" key={e.name.toString()}>
+                                            <Link href={e.link ? e.link : "#"} title={e.name} className="outline-none" key={e.name?.toString()}>
                                                 {/* <a title={e.name} className="outline-none"> */}
                                                     <li className="text-[14px] text-[#344054] marker:text-[#9295A4] px-3 py-2 rounded hover:bg-[#ececee87]">
                                                         {e.name}
