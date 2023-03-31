04fc0b287e0b36f33a860b9d15e5862d4b4b9f15
Paul <trantuanphong2410@gmail.com>
2023-03-31 17:50:32 +0700

    update

--- components/UI/popup.js
+++ components/UI/popup.js
@@ -1,26 +1,36 @@
 import React from "react";
 import Popup from "reactjs-popup";
+import Head from "next/head";
 
 import {Add as IconClose} from "iconsax-react"
 
 const Index = (props) => {
   return (
     <React.Fragment>
-        <button className={props.classNameBtn} onClick={props.onClickOpen}>{props.button}</button>
-        <Popup 
-            open={props.open}
-            closeOnDocumentClick={false}
-            onClose={props.onClose}
-            className="popup-edit"
-        >
-            <div className=" bg-[#ffffff] p-4 shadow-xl rounded-xl 2xl:mt-28 mt-36">
-                <div className="text-[#101828] font-medium text-xl flex justify-between items-center">
-                    <h1>{props.title}</h1>
-                    <button onClick={props.onClose} className="outline-none w-7 h-7 bg-slate-200 hover:opacity-80 hover:scale-105 transition rounded-full flex flex-col justify-center items-center"><IconClose className="rotate-45" /></button>
+        <Head>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap" rel="stylesheet"/>
+        </Head>
+        <React.Fragment>
+            <button className={props.classNameBtn} onClick={props.onClickOpen}>{props.button}</button>
+            <Popup 
+                open={props.open}
+                closeOnDocumentClick={false}
+                onClose={props.onClose}
+                className="popup-edit"
+                contentStyle={{
+                    fontFamily: 'Lexend Deca, sans-serif'
+                }}
+            >
+                <div className=" bg-[#ffffff] p-4 shadow-xl rounded-xl 2xl:mt-28 mt-36">
+                    <div className="text-[#101828] font-medium text-xl flex justify-between items-center">
+                        <h1>{props.title}</h1>
+                        <button onClick={props.onClose} className="outline-none w-7 h-7 bg-slate-200 hover:opacity-80 hover:scale-105 transition rounded-full flex flex-col justify-center items-center"><IconClose className="rotate-45" /></button>
+                    </div>
+                    {props.children}
                 </div>
-                {props.children}
-            </div>
-        </Popup>
+            </Popup>
+        </React.Fragment>
     </React.Fragment>
   );
 };
