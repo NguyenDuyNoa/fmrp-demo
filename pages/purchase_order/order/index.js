fa2166ecc8e965abe27afcffcc2c221842e2f971
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-05-18 10:48:57 +0700

    upted

--- pages/purchase_order/order/index.js
+++ pages/purchase_order/order/index.js
@@ -738,7 +738,7 @@ return (
      
   </div>  
           {/* <div className="mt-4 space-x-5 w-[999px] 2xl:h-[550px] xl:h-[750px] h-[700px] customsroll overflow-hidden  3xl:h-auto 2xl:scrollbar-thin 2xl:scrollbar-thumb-slate-300 2xl:scrollbar-track-slate-100">         */}
-          <div className="mt-4 space-x-5 w-[999px] 2xl:h-[750px] xl:h-[750px] h-[700px] overflow-hidden">        
+          <div className="mt-4 space-x-5 w-[999px] 2xl:h-[750px] xl:h-[750px] h-[700px]">        
           <div>
            <div className='w-[999px]'>
              <div  className="min:h-[170px] h-[72%] max:h-[100px]  customsroll overflow-auto pb-2 scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-100">
