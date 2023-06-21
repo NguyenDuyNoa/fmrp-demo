de93f7d374e53c7d4d3101d18f8abe18905da57b
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-06-21 15:56:48 +0700

    updated

--- pages/purchase_order/import/index.js
+++ pages/purchase_order/import/index.js
@@ -892,7 +892,7 @@ const Popup_Pdf =(props)=>{
 
   const fetchDataSettingsCompany = async () => {
     if (props?.id) {
-      await  Axios("GET", `/api_web/Api_Setting/CompanyInfo?csrf_protection=true`, {}, (err, response) => {
+      await  Axios("GET", `/api_web/Api_setting/CompanyInfo?csrf_protection=true`, {}, (err, response) => {
           if(!err){
                   var {data} =  response.data
                   setDataCompany(data)
