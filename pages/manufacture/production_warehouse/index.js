a7cb22a79cdadfdef34da8e341a8cb91c9f9b5da
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-08-08 14:59:51 +0700

    updated

--- pages/manufacture/production_warehouse/index.js
+++ pages/manufacture/production_warehouse/index.js
@@ -509,7 +509,7 @@ const Index = (props) => {
             </Head>
             <div className="3xl:pt-[88px] 2xl:pt-[74px] xl:pt-[60px] lg:pt-[60px] 3xl:px-10 3xl:pb-10 2xl:px-10 2xl:pb-8 xl:px-10 xl:pb-10 lg:px-5 lg:pb-10 space-y-1 overflow-hidden h-screen">
                 {/* {data_export.length > 0 && <Popup_status className="hidden" data_export={data_export} dataLang={dataLang}/>} */}
-                <div className="flex space-x-3 xl:text-[14.5px] text-[12px]">
+                <div className="flex space-x-3 mt-3 xl:text-[14.5px] text-[12px]">
                     <h6 className="text-[#141522]/40">
                         {dataLang?.production_warehouse ||
                             "production_warehouse"}
