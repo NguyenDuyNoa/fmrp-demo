139dad61c1c1149bbc787251169073df1bf78922
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-09-06 17:34:58 +0700

    updated

--- services/URL.js
+++ services/URL.js
@@ -1,2 +1,3 @@
-export const ulrExel = "https://demo.fososoft.com/FMRP";
+// export const ulrExel = "https://demo.fososoft.com/FMRP";
 // export const ulrExel = "http://192.168.1.178/FMRP";
+export const ulrExel = "https://fmrp.fososoft.com/";
