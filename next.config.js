daa9bdc902aa8caad2c3e24638f3e0ae35c0ffb3
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-09-22 17:48:37 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["localhost", "demo.fososoft.com", "192.168.1.178"],
+        domains: ["localhost", "demo.fososoft.com", "192.168.1.178", "fmrp.fososoft.com"],
     },
 };
 
