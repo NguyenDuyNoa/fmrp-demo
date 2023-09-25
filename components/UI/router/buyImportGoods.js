bb9b3b6b58af555bc065ab64f511b66f2f622c33
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-09-25 17:39:20 +0700

    updated

--- components/UI/router/buyImportGoods.js
+++ components/UI/router/buyImportGoods.js
@@ -1,29 +1,30 @@
 // Yêu cầu mua hàng
+const url = "/purchase_order";
 
 const routerPurchases = {
-    home: "/purchase_order/purchases?tab=",
-    form: "/purchase_order/purchases/form",
+    home: `${url}/purchases?tab=`,
+    form: `${url}/purchases/form`,
 };
 
 // Đơn đặt hàng PO
 
 const routerOrder = {
-    home: "/purchase_order/order?tab=",
-    form: "/purchase_order/order/form",
+    home: `${url}/order?tab=`,
+    form: `${url}/order/form`,
 };
 
 // Nhập hàng
 
 const routerImport = {
-    home: "/purchase_order/import?tab=",
-    form: "/purchase_order/import/form",
+    home: `${url}/import?tab=`,
+    form: `${url}/import/form`,
 };
 
 // Trả hàng
 
 const routerReturns = {
-    home: "/purchase_order/returns?tab=",
-    form: "/purchase_order/returns/form",
+    home: `${url}/returns?tab=`,
+    form: `${url}/returns/form`,
 };
 
 export { routerPurchases, routerOrder, routerImport, routerReturns };
--- components/UI/router/sellingGoods.js
+++ components/UI/router/sellingGoods.js
@@ -1,13 +1,14 @@
 // Trả lại hàng bán
+const url = "/sales_export_product";
 const routerReturnSales = {
-    home: "/sales_export_product/returnSales",
-    form: "/sales_export_product/returnSales/form",
+    home: `${url}/returnSales`,
+    form: `${url}/returnSales/form`,
 };
 
 // Phiếu giao hàng
 
 const routerDeliveryReceipt = {
-    home: "/sales_export_product/deliveryReceipt",
-    form: "/sales_export_product/deliveryReceipt/form",
+    home: `${url}/deliveryReceipt`,
+    form: `${url}/deliveryReceipt/form`,
 };
 export { routerReturnSales, routerDeliveryReceipt };
