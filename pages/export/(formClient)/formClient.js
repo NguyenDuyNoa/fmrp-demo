92e051e1c7d92b6731136b7219a4fea28cba614b
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-10-05 11:47:00 +0700

    upted

--- pages/export/(formClient)/formClient.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from "react";
-import Select from "react-select";
-const FormClient = ({ dataLang }) => {
-    return <div></div>;
-};
-export default FormClient;
