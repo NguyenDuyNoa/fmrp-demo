de30337e1bfc813dceeb3b3a9609692ee80ccf4a
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-10-07 18:08:28 +0700

    updated

--- pages/manufacture/productionOverview/index.js
+++ pages/manufacture/productionOverview/index.js
@@ -1214,7 +1214,7 @@ const Index = (props) => {
     return (
         <>
             <Head>
-                <title>{dataLang?.productsWarehouse_title || "productsWarehouse_title"}</title>
+                <title>{"Tổng quan sản xuất"}</title>
             </Head>
             <div className="relative  3xl:pt-[88px] xxl:pt-[80px] 2xl:pt-[78px] xl:pt-[75px] lg:pt-[70px] pt-70 3xl:px-10 3xl:pb-10 2xl:px-10 2xl:pb-8 xl:px-10 xl:pb-10 lg:px-5 lg:pb-10 space-y-1 overflow-hidden h-screen">
                 {trangthaiExprired ? <div className="p-4"></div> : <Header {...propsDetail} />}
