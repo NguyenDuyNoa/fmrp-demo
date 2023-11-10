1b4daac9d77444920a3a67f37a96cd2d56411cf7
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-11-10 16:26:18 +0700

    updated

--- pages/sales_export_product/salesOrder/(PopupDetail)/PopupKeepStock.js
+++ pages/sales_export_product/salesOrder/(PopupDetail)/PopupKeepStock.js
@@ -502,7 +502,9 @@ const Popup_KeepStock = ({ dataLang, status, id, onRefresh, ...props }) => {
                                                     <h6
                                                         className={`2xl:text-[13px] flex items-start ${
                                                             e?.item?.warehouse_location?.length > 0
-                                                                ? "justify-start"
+                                                                ? e?.item?.warehouse_location?.length == 1
+                                                                    ? "justify-center"
+                                                                    : "justify-start"
                                                                 : "justify-center"
                                                         }  flex-wrap gap-2 xl:text-[12px] text-[11px] py-0.5 col-span-3  rounded-md  break-words`}
                                                     >
