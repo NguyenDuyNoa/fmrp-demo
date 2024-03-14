5b563c9d64e2c996519826fbab32bc301fe1b8bc
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2024-03-14 11:13:08 +0700

    updated

--- components/UI/common/Customscrollbar.js
+++ components/UI/common/Customscrollbar.js
@@ -2,7 +2,7 @@ export const Customscrollbar = (props) => {
     return (
         <div className={`${props.className ?
             props.className :
-            "min:h-[200px] 3xl:h-[83%] xxl:h-[74%] 2xl:h-[76%] xl:h-[72%] lg:h-[82%] max:h-[400px] pb-2"}
+            "min:h-[200px] 3xl:h-[83%] xxl:h-[76%] 2xl:h-[79%] xl:h-[74%] lg:h-[84%] max:h-[400px] pb-2"}
              overflow-x-hidden overflow-y-auto
             scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-100`}>
             {props.children}
