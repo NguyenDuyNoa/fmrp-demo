ac543b0ce61fb439baf8de90a3ebb2ac77fa84ec
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2024-06-24 17:41:19 +0700

    updated

--- components/UI/common/Tag/TagBranch.jsx
+++ components/UI/common/Tag/TagBranch.jsx
@@ -1,7 +1,8 @@
-export const TagBranch = (props) => {
+const TagBranch = (props) => {
     return (
         <div key={props?.key} {...props} className={`${props?.className} 3xl:text-[13px] 2xl:text-[10px] xl:text-[9px] text-[8px] text-[#0F4F9E] font-[300] px-1.5 py-0.5 border border-[#0F4F9E] bg-white rounded-[5.5px] uppercase`}>
             {props.children}
         </div>
     )
 }
+export default TagBranch
