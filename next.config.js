60a64f6102965957eca967abcd7e69c1a700907c
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2025-03-05 12:43:04 +0700

    updated config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["localhost", "demo.fososoft.com", "192.168.1.178", "fmrp.fososoft.com"],
+        domains: ["localhost", "demo.fososoft.com", "192.168.1.178", "fmrp.fososoft.com", 'admin.fmrp.vn'],
     },
     env: {
         customKey: "my-value",
