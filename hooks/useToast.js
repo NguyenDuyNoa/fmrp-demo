9d8f35f105049d1cb9eecaa5bf60ecc8027acc9a
nguyễn quốc sỹ <quocsy2511@gmail.com>
2025-04-17 10:45:38 +0700

    fix: ui alter

--- hooks/useToast.js
+++ hooks/useToast.js
@@ -1,18 +1,34 @@
 import Swal from "sweetalert2";
+import 'animate.css';
+
 const useToast = () => {
     const Toast = Swal.mixin({
         toast: true,
-        position: "top-end",
+        position: "top",
         showConfirmButton: false,
-        timer: 2000,
-        timerProgressBar: true,
+        // timer: 1000,
+        // timerProgressBar: true,
+        showClass: {
+            popup: 'animate__zoomIn faster',
+        },
+        hideClass: {
+            popup: 'animate__zoomOut faster',
+        },
     });
 
     const showToast = (type, message, time) => {
         Toast.fire({
             icon: type,
             title: message,
-            timer: time || 3000,
+            timer: time || 100000,
+            iconHtml: type === "success" ? `<div class="custom-check-icon-success">✓</div>` : `<div class="custom-check-icon-error">x</div>`,
+            customClass: {
+                popup: `custom-toast-popup custom-toast-popup-${type}`,
+                title: `custom-toast-title-${type} custom-toast-title`,
+                icon: "custom-toast-icon",
+                closeButton: `custom-toast-close-button-${type} .custom-toast-close-button`
+            },
+            showCloseButton: true
         });
     };
 
